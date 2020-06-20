e7dd764a12ad3f4098e4e774821d0c68c56733b8
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-20 04:30:13 -0400

    solutions

---
+++ longestpalindromicsubstring.js
@@ -0,0 +1,36 @@
+//Objective is to find the longest palindrome in a given string.
+
+let string = "babad"
+
+
+//O(n^2) solution that expands around the center and uses two pointers
+//to update the longest palindromic string.
+
+let start = 0
+let end = string.length - 1
+let max = 0
+
+for (let i = 0; i < string.length; i++) {
+    let tempStart = i
+    let tempEnd = i
+
+    //continue until you find a character that's not the same as given index
+    while (string[i] == string[tempEnd + 1]) {
+        tempEnd++
+    }
+
+    //expand left and rgith as long as the ends are the same
+    while (tempStart >= 0 && tempEnd < string.length && string[tempStart - 1] == string[tempEnd + 1]) {
+        tempStart--
+        tempEnd++
+    }
+
+    //If a new longest substring is found, update the start and end
+    if (max < tempEnd - tempStart + 1) {
+        start = tempStart 
+        end = tempEnd 
+        max = tempEnd - tempStart + 1
+    }
+}
+
+return string.substring(start, end + 1)
