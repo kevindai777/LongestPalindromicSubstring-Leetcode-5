1fcb3c14a8d3df18eaebdd229a7bb36e601647d9
kevindai777 <66235906+kevindai777@users.noreply.github.com>
2020-06-23 15:05:52 -0400

    edits to explanation

--- longestpalindromicsubstring.js
+++ longestpalindromicsubstring.js
@@ -15,11 +15,13 @@ for (let i = 0; i < string.length; i++) {
     let tempEnd = i
 
     //continue until you find a character that's not the same as given index
+    //(expand width of center)
     while (string[i] == string[tempEnd + 1]) {
         tempEnd++
     }
 
-    //expand left and rgith as long as the ends are the same
+    //expand left and right as long as the ends are the same
+    //(expand around center)
     while (tempStart >= 0 && tempEnd < string.length && string[tempStart - 1] == string[tempEnd + 1]) {
         tempStart--
         tempEnd++
